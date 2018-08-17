762643c098591a6037dde82684d41d41baf31a32
kennethbrent <kennethbrentthomas@gmail.com>
2018-08-17 11:26:04 -0400

    Added modal

---
+++ js/scripts.js
@@ -0,0 +1,264 @@
+ $.ajax({
+  url: 'https://randomuser.me/api/?results=12&inc=picture,name,email,location,nat,dob,cell&nat=us',
+  dataType: 'json',
+  success: function(data) {
+	  appendUserCards(data);
+	  appendModaldata(data);
+	  for(let i = 0; i <= 11; i ++) {
+	showUserModal(i);
+}
+	  
+  }
+});
+//////////////////////////////////////////////
+//append cards div and user data/////////////
+////////////////////////////////////////////
+function appendUserCards(data) {
+	for(let i = 0; i < data.results.length; i++){
+		$('.gallery').append(
+	  		`<div class="card">
+				<div class="card-img-container">
+                	<img class="card-img" src=${data.results[i].picture.medium} alt="profile picture">
+				</div>
+				<div class="card-info-container">
+					<h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                	<p class="card-text">${data.results[i].email}</p>
+                	<p class="card-text cap">${data.results[i].location.city} ${data.results[i].location.state}</p>
+           		</div>
+       		</div>`
+			);
+			
+	  }
+
+}
+
+//////////////////////////////
+/////////modal array//
+//////////////////////
+
+const modalArray = [];
+function appendModaldata(data) {
+	for(let i = 0; i < data.results.length; i++) {
+		const userDOB = data.results[i].dob.date;
+		const formmatedDOB = userDOB.slice(0,10);
+		modalArray.push(`
+			<div class='modalArray' id='modalArray${i}'>
+				<img class="modal-img" src=${data.results[i].picture.medium}>
+				<h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+				<p class="modal-text">${data.results[i].email}</p>
+				<p class="modal-text cap">${data.results[i].location.city}</p>
+				<hr>
+				<p class="modal-text">${data.results[i].cell}</p>
+				<p class="modal-text">${data.results[i].location.street} ${data.results[i].location.city} ${data.results[i].location.state}</p>
+				<p class="modal-text">${formmatedDOB}</p>
+			</div>
+`);
+					
+}
+
+	////////////////////////////////////////
+	//Append modal data to array//////
+	
+	for(let i = 0; i < modalArray.length; i++) {
+	$('.modal-info-container').append(modalArray[i]);
+		$('.modalArray').hide();
+	}
+
+
+}
+/////////////////////////////////////////////////////
+//Append Modal containers and buttons///////////
+////////////////////////////////////////////////////
+$('body').append(`
+	<div class='modal-container'>
+		<div class='modal'>
+			<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+			<div class='modal-info-container'>
+			
+			</div>
+		</div>
+		
+		<div class="modal-btn-container">
+           <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+           <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+	</div>
+
+`);
+$('.modal-container').hide();
+//
+//$('.modal-container').hide();
+/////////////////////////////////////////////////////////////
+/////////Function to close modal//////////////////////
+//////////////////////////////////////////////
+function closeModal() {
+	$('.modal-close-btn').on('click', () => {
+		$('.modal-container').hide();
+		$('.modalArray').hide();
+	});
+}
+closeModal();
+
+////////////////////////////////////////////
+///function  to show modal on click///////
+////////////////////////////////////////
+const userCard = document.getElementsByClassName('card')
+function showUserModal (arrayIndex) {
+	$(userCard[arrayIndex]).on('click', function() {
+		$('.modal-container').show();
+		$('#modalArray' + arrayIndex).show();
+		$('.modal-close-btn').on('click', () => {
+			$('#modalArray' + arrayIndex).hide();
+		});
+		
+	});
+}
+
+////////////////////////////////////////////////////
+////Toggle users in Modal Window//////////////////
+//////////////////////////////////////////////////
+$('#modal-next').on('click', function() {
+		toggleNext();
+});
+
+$('#modal-prev').on('click', function() {
+		togglePrev();
+});
+
+function toggleNext(){
+	if($('#modalArray0').is(":visible")) {
+	   	$('#modalArray0').hide();
+		$('#modalArray0').next().show();
+	   } else if($('#modalArray1').is(":visible")) {
+	   	$('#modalArray1').hide();
+		$('#modalArray1').next().show();
+	   } else if($('#modalArray2').is(":visible")) {
+	   	$('#modalArray2').hide();
+		$('#modalArray2').next().show();
+	   } else if($('#modalArray3').is(":visible")) {
+	   	$('#modalArray3').hide();
+		$('#modalArray3').next().show();
+	   } else if($('#modalArray4').is(":visible")) {
+	   	$('#modalArray4').hide();
+		$('#modalArray4').next().show();
+	   } else if($('#modalArray5').is(":visible")) {
+	   	$('#modalArray5').hide();
+		$('#modalArray5').next().show();
+	   } else if($('#modalArray6').is(":visible")) {
+	   	$('#modalArray6').hide();
+		$('#modalArray6').next().show();
+	   } else if($('#modalArray7').is(":visible")) {
+	   	$('#modalArray7').hide();
+		$('#modalArray7').next().show();
+	   } else if($('#modalArray8').is(":visible")) {
+	   	$('#modalArray8').hide();
+		$('#modalArray8').next().show();
+	   } else if($('#modalArray9').is(":visible")) {
+	   	$('#modalArray9').hide();
+		$('#modalArray9').next().show();
+	   } else if($('#modalArray10').is(":visible")) {
+	   	$('#modalArray10').hide();
+		$('#modalArray10').next().show();
+	   } 
+}
+
+function togglePrev(){
+	if($('#modalArray1').is(":visible")) {
+	   	$('#modalArray1').hide();
+		$('#modalArray1').prev().show();
+	   } else if($('#modalArray2').is(":visible")) {
+	   	$('#modalArray2').hide();
+		$('#modalArray2').prev().show();
+	   } else if($('#modalArray3').is(":visible")) {
+	   	$('#modalArray3').hide();
+		$('#modalArray3').prev().show();
+	   } else if($('#modalArray4').is(":visible")) {
+	   	$('#modalArray4').hide();
+		$('#modalArray4').prev().show();
+	   } else if($('#modalArray5').is(":visible")) {
+	   	$('#modalArray5').hide();
+		$('#modalArray5').prev().show();
+	   } else if($('#modalArray6').is(":visible")) {
+	   	$('#modalArray6').hide();
+		$('#modalArray6').prev().show();
+	   } else if($('#modalArray7').is(":visible")) {
+	   	$('#modalArray7').hide();
+		$('#modalArray7').prev().show();
+	   } else if($('#modalArray8').is(":visible")) {
+	   	$('#modalArray8').hide();
+		$('#modalArray8').prev().show();
+	   } else if($('#modalArray9').is(":visible")) {
+	   	$('#modalArray9').hide();
+		$('#modalArray9').prev().show();
+	   } else if($('#modalArray10').is(":visible")) {
+	   	$('#modalArray10').hide();
+		$('#modalArray10').prev().show();
+	   } else if($('#modalArray11').is(":visible")) {
+		   	$('#modalArray11').hide();
+	   		$('#modalArray11').prev().show();
+	   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function appendModal(data) {
+//	 $('.card').on('click', function(){
+//	    $('body').append(
+//		 `<div class="modal-container">
+//			<div class="modal">
+//				<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//				<div class="modal-info-container">
+//					<img class="modal-img" src='yeyh' alt="profile picture">
+//					<h3 id="name" class="modal-name cap">name</h3>
+//					<p class="modal-text">email</p>
+//					<p class="modal-text cap">city</p>
+//					<hr>
+//					<p class="modal-text">(555) 555-5555</p>
+//					<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+//					<p class="modal-text">Birthday: 10/21/2015</p>
+//				 </div>
+//			 </div>
+//
+//						// IMPORTANT: if you're not going for exceeds 
+//			  <div class="modal-btn-container">
+//				  <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+//				  <button type="button" id="modal-next" class="modal-next btn">Next</button>
+//			 </div>
+//		 </div>`
+//		);
+//		 closeModul();
+//	});
+//}
+//
+
+
+
